6f5d17de8c72d3d02c1620d1c540103707a64b2b
anonymouse777 <muhammadusman2027@hotmail.com>
2022-03-10 03:21:55 +0500

    overflow-x:scroll

--- src/App.js
+++ src/App.js
@@ -1,42 +1,28 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
-
-import swim from './images/swimmer.png';
-import wedding from './images/wedding.png';
-import bike from './images/bike.png';
+import data from "./Data"
 
 export default function App() {
+    let result = data.map(function (item) {
+        return (
+            <Card 
+                key = {item['id']}
+                image = {item['image']}
+                rating = {item['rating']}
+                count = {item['count']}
+                lesson = {item['lesson']}
+                price = {item['price']}
+            />
+        )
+    })
     return (
         <div>
             <Navbar />
             <Hero />
-            <div className="card_container">
-
-                <Card 
-                    image={swim} 
-                    lesson="Life lessons with Katie Zaferes" 
-                    rating="5.0" 
-                    count="6" 
-                    price="136" 
-                />
-                
-                <Card 
-                    image={wedding} 
-                    lesson="Learn wedding photography" 
-                    rating="5.0" 
-                    count="30" 
-                    price="125" 
-                />
-                
-                <Card 
-                    image={bike} 
-                    lesson="Group Mountain Biking" 
-                    rating="4.8" 
-                    count="2" 
-                    price="50" 
-                />
-            </div>
+            <section className="card_container">
+                {result}
+            </section>
         </div>
     )
 }
---
+++ src/Data.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        'id': 1,
+        'image': './images/swimmer.png', 
+        'lesson': "Life lessons with Katie Zaferes",
+        'rating': 5.0,
+        'count': 6,
+        'price': 136, 
+    },
+    {
+        'id': 2,
+        'image': './images/wedding.png',
+        'lesson': "Learn wedding photography",
+        'rating': 5.0,
+        'count': 30,
+        'price': 125,
+    },
+    {
+        'id': 3,
+        'image': './images/bike.png',
+        'lesson': "Group Mountain Biking",
+        'rating': 4.8,
+        'count': 2,
+        'price': 50,
+    }
+]
